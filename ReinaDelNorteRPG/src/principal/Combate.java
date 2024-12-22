40472a61d2e3a4f61641376702e20ec3a988cef0
DevepMau <oshiettecs@gmail.com>
2024-12-22 14:26:50 -0300

    Creacion del tablero de combate basico inicial

--- ReinaDelNorteRPG/src/principal/Combate.java
+++ ReinaDelNorteRPG/src/principal/Combate.java
@@ -1,40 +1,80 @@
 package principal;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.util.HashMap;
 
 public class Combate {
 	PanelDeJuego pdj;
 	Graphics2D g2;
-	HashMap<Integer, Zona> zona = new HashMap<>();
+	HashMap<Integer, Zona> zonas = new HashMap<>();
 	
 	public Combate(PanelDeJuego pdj) {
 		this.pdj = pdj;
 		crearTablero();
 		
 	}
+	/////////////////////////////////////////////////////////
 	public void actualizar() {}
 	
 	public void dibujar(Graphics2D g2) {
 		this.g2 = g2;
-		for (Zona zona : zona.values()) {
+		for (Zona zona : zonas.values()) {
 			zona.dibujar(g2);
 		}
+		//dibujarRetrato(zonas.get(32).x, zonas.get(32).y, pdj.tamañoDeBaldosa*4, pdj.tamañoDeBaldosa*4);
 	}
-	
+	//////////////////////////////////////////////////////////
 	public void crearTablero() {
 		int x = 0;
 		int y = 0;
-		int id = 0;
+		int n = 0;
+		int id = 1;
 		for (int i = 0; i < 6; i++) {
 			for (int k = 0; k < 8; k++) {
-				zona.put(id, new Zona(x, y, pdj));
-				id++;
+				if(verificar(n)) {
+					if(n == 32) {
+						zonas.put(0, new Zona(0, x, y, pdj));
+					}
+					else {
+						zonas.put(id, new Zona(id, x, y, pdj));
+						id++;
+					}
+				}
+				n++;
 				x += pdj.tamañoDeBaldosa*2;
 			}
 			x = 0;
 			y += pdj.tamañoDeBaldosa*2;
 		}
+	}
+	
+	public void dibujarVentana(int x, int y, int width, int height) {
+		Color c = new Color(0,0,0, 200);
+		g2.setColor(c);
+		g2.fillRoundRect(x+5, y+5, width, height, 35, 35);
+		
+		c = new Color(255, 255, 255);
+		g2.setColor(c);
+		g2.setStroke(new BasicStroke(5));
+		g2.drawRoundRect(x+5, y+5, width-10, height-10, 25, 25);
+	}
+	
+	public void dibujarRetrato(int x, int y, int width, int height) {
+		Color c = new Color(135, 206, 235);
+		g2.setColor(c);
+		g2.setStroke(new BasicStroke(5));
+		g2.fillRoundRect(x+width/4, y+5, width/2, height/2, 100, 100);
+		g2.fillRect(x+width/4, y+height/2, width/2, height/2);
+		g2.drawRoundRect(x+5, y+5, width-10, height-10, 25, 25);
+	}
+	
+	public boolean verificar(int n) {
+		if((10 <= n && n <= 13) || (34 <= n && n <= 37) || n == 32) {
+			return true;
+		}
+		return false;
 	}
 
 }
--- ReinaDelNorteRPG/src/principal/Zona.java
+++ ReinaDelNorteRPG/src/principal/Zona.java
@@ -1,6 +1,7 @@
 package principal;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 
 public class Zona {
@@ -10,15 +11,25 @@ public class Zona {
 	public int y;
 	public int id;
 	
-	public Zona(int x, int y, PanelDeJuego pdj) {
+	public Zona(int id,int x, int y, PanelDeJuego pdj) {
 		this.x = x;
 		this.y = y;	
+		this.id = id;
 		this.pdj = pdj;
 	}
 	
 	public void dibujar(Graphics2D g2) {
 		this.g2 = g2;
 		g2.setColor(Color.white);
-		g2.drawRect(x, y, pdj.tamañoDeBaldosa*2, pdj.tamañoDeBaldosa*2);
+		g2.setFont(g2.getFont().deriveFont(Font.BOLD, 16f));
+		if(id == 0) {
+			g2.drawRect(x, y, pdj.tamañoDeBaldosa*3, pdj.tamañoDeBaldosa*4);
+			g2.drawString("portrait", x+pdj.tamañoDeBaldosa-6, y+pdj.tamañoDeBaldosa*2+8);
+		}
+		else {
+			g2.drawRect(x, y, pdj.tamañoDeBaldosa*2, pdj.tamañoDeBaldosa*2);
+			g2.drawString(String.valueOf(id), x+pdj.tamañoDeBaldosa-8, y+pdj.tamañoDeBaldosa+8);
+		}
+		
 	}
 }
