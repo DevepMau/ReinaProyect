8587b397e2dc091c959fe7bf5c3fab25433d241d
DevepMau <oshiettecs@gmail.com>
2025-02-15 18:27:45 -0300

    Se modifico la unidad NovataTimida. Ahora ya no evoluciona sino que maldice a los enemigos que ataca, maldice a quien le ataca y gana escudos iniciales segun la cantidad de aliadas de MHJ en el equipo

--- ReinaDelNorteRPG/src/unidades/NovataTimida.java
+++ ReinaDelNorteRPG/src/unidades/NovataTimida.java
@@ -2,14 +2,15 @@ package unidades;
 
 import java.util.ArrayList;
 
+import principal.Habilidades;
 import principal.PanelDeJuego;
 import principal.Zona;
 
 public class NovataTimida extends Unidad{
 	
-	private int spHabilidad1;
-	private int cargas;
 	private String[] listaDeHabilidades = new String[1];
+	private boolean habilitar = true;
+	private int escudosIniciales = 0;
 
 	public NovataTimida(Zona zona, boolean aliado, PanelDeJuego pdj) {
 		super(zona, aliado, pdj);
@@ -18,131 +19,33 @@ public class NovataTimida extends Unidad{
 		this.setClase("Novata Timida");
 		this.setIdFaccion(4);
 		this.setGenero(0);
-		this.setHPMax(obtenerValorEntre(40,50));
+		this.setHPMax(obtenerValorEntre(60,75));
 		this.setHP(this.getHPMax());
 		this.setSPMax(0);
 		this.setSP(this.getSPMax());
-		this.setAtq(obtenerValorEntre(6,9));
-		this.setDef(obtenerValorEntre(2,5));
+		this.setAtq(obtenerValorEntre(6,8));
+		this.setDef(obtenerValorEntre(10,20));
 		this.setPCRT(0);
 		this.setDCRT(1.5);
-		this.setEva(0);
-		this.setVel(obtenerValorEntre(4,12));
-		this.setEscudos(0);
-		this.cargas = 0;
-		this.spHabilidad1 = 12;
+		this.setEva(15);
+		this.setVel(obtenerValorEntre(10,20));
+		this.setEscudos(1);
 		this.listaDeHabilidades[0] = "...";
 		this.generarCuerpo();
 	}
 	//METODO PRINCIPAL//////////////////////////////////////////////////////////////////
-	public void realizarAccion(ArrayList<Unidad> enemigos, ArrayList<Unidad> aliados) {
-		if(this.getClase() != "Novata Timida") {
-			if(cumpleReqDeHab1()) {
-				this.setHabilidadElegida(0);
-				usarHabilidadEnemigo(enemigos);
-				
-			}
-			else {
-				realizarAtaqueEnemigo(enemigos);
-			}
-		}
-		else {
-			realizarAtaqueEnemigo(enemigos);
-		}
-		this.pasivaDeClase(aliados, enemigos);
-	}
 	//METODOS ENEMIGO////////////////////////////////////////////////////////////////////
-	public void usarHabilidadEnemigo(ArrayList<Unidad> unidades) {
-		if(this.getHabilidadElegida() == 0) {
-			Unidad unidad = elegirObjetivo(unidades);
-			iniciativa(unidad);	
-			if(this.cargas == 2) {
-				if(this.getClase() != "Novata Confiable") {
-					evolucionar();
-				}
-			}
-			else {
-				this.cargas++;
-			}
-		}
-	}
 	public void realizarAtaqueEnemigo(ArrayList<Unidad> unidades) {
 		if(!unidades.isEmpty()) {
 			super.realizarAtaqueEnemigo(unidades);
-			if(this.cargas == 2) {
-				if(this.getClase() != "Novata Confiable") {
-					evolucionar();
-				}
-			}
-			else {
-				this.cargas++;
-			}
 		}
 	}
 	//METODOS JUGADOR////////////////////////////////////////////////////////////////////
 	public void realizarAtaque(Unidad unidad, ArrayList<Unidad> unidades) {
 		super.realizarAtaque(unidad, unidades);
-		if(this.cargas == 2) {
-			if(this.getClase() != "Novata Confiable") {
-				evolucionar();
-			}
-		}
-		else {
-			this.cargas++;
-		}
-	}
-	public void usarHabilidad(Unidad unidad, ArrayList<Unidad> unidades) {
-		iniciativa(unidad);
-		if(this.cargas == 2) {
-			if(this.getClase() != "Novata Confiable") {
-				evolucionar();
-			}
-		}
-		else {
-			this.cargas++;
-		}
 	}
 	//HABILIDADES////////////////////////////////////////////////////////////////////////
-	public void evolucionar() {
-		if(this.getClase() == "Novata Timida") {
-			this.setClase("Novata Cauta");
-			this.listaDeHabilidades[0] = "INICIATIVA";
-		}
-		else if(this.getClase() == "Novata Cauta") {
-			this.setClase("Novata Confiable");
-		}
-		this.setHPMax(this.getHPMax() + this.getHPMax()/4);
-		this.restaurarHP(this.getHPMax()/4);
-		this.setSPMax(this.getSPMax() + 30);
-		this.restaurarSP(this.getSPMax());
-		this.setAtq(this.getAtq() + 3);
-		this.setDef(this.getDef() + 1);
-		this.setVel(this.getVel() + 3);
-		this.setEva(this.getEva() + 0.05);
-		this.setPCRT(this.getPCRT() + 0.05);
-		this.generarCuerpo();
-		this.cargas = 0;
-	}
-	public void iniciativa(Unidad unidad) {
-		if(unidad != null) {
-			this.setSP(this.getSP() - this.spHabilidad1);
-			int daño = Math.max(1, ((this.getAtq()) + this.getAtqMod()) - (unidad.getDef() + unidad.getDefMod()));
-			unidad.recibirDaño(daño, true, 20);
-			this.setEscudos(this.getEscudos() + 1);
-			contarFaltas(unidad, 1);
-		}	
-	}
 	//METODOS AUXILIARES/////////////////////////////////////////////////////////////////
-	public boolean cumpleReqDeHab1() {
-		if(this.getClase() != "Novata Timida") {
-			if(this.getSP() > 0) {
-				if(this.getSP() >= this.spHabilidad1) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
 	public void configurarTipoDeaccion() {
 		if(this.getHabilidadElegida() == 0) {
 			this.setObjetivoUnico(true);
@@ -150,6 +53,20 @@ public class NovataTimida extends Unidad{
 		}
 		else {
 			this.setAccion("");
+		}
+	}
+	public void pasivaDeClase(ArrayList<Unidad> aliados, ArrayList<Unidad> enemigos) {
+		super.pasivaDeClase(aliados, enemigos);
+		if(habilitar) {
+			if(!aliados.isEmpty()) {
+				for(Unidad unidad : aliados) {
+					if(unidad.getIdFaccion() == 4 && !this.equals(unidad)) {
+						this.escudosIniciales++;
+					}
+				}
+				Habilidades.protegerUnidad(this, this.escudosIniciales, pdj);
+				habilitar = false;
+			}
 		}
 	}
 	public String[] getListaDeHabilidades() {return listaDeHabilidades;}
